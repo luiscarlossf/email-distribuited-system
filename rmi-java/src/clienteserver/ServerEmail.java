21423cfa2f695a21544e2efea4f54907528872b8
Luis <luiscarlos.sf2017@gmail.com>
2019-04-19 19:36:20 -0300

    Add list() in server

--- rmi-java/src/clienteserver/ServerEmail.java
+++ rmi-java/src/clienteserver/ServerEmail.java
@@ -3,7 +3,9 @@ package clienteserver;
 import java.rmi.*;
 import java.rmi.server.*;
 import java.util.ArrayList;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.FileWriter;
@@ -60,13 +62,52 @@ public class ServerEmail extends UnicastRemoteObject implements ServerEmailInter
 
 	@Override
 	public ArrayList<Email> list(String username) throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		Email email;
+		File directory = new File("./"+username);
+		ArrayList<Email> emails= new ArrayList<Email>();
+        
+		if(!directory.exists()){
+			boolean statusdir = directory.mkdir();
+			if (statusdir){
+				System.out.println("O diretório foi criado para "+ email.getRecipient());
+			}else{
+				System.out.println("Erro! O diretório não foi criado para "+ email.getRecipient());
+			}
+		}
+        try{
+			for(File file: directory.listFiles()){
+				FileReader fileReader = new FileReader(file);
+
+				BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+				String linha = "";
+
+				email = new Email();
+				if ((linha=bufferedReader.readLine()) != null)
+					email.setId(Double.parseDouble(linha));
+				if ((linha=bufferedReader.readLine()) != null)
+					email.setSender(linha);
+				if ((linha=bufferedReader.readLine()) != null)
+					email.setRecipient(linha);
+				if ((linha=bufferedReader.readLine()) != null)
+					email.setSubject(linha);
+				if ((linha=bufferedReader.readLine()) != null)
+					email.setBody(linha);
+				
+				emails.add(email);
+				
+				fileReader.close();
+			    bufferedReader.close();
+			}
+	    }catch(IOException e){
+			e.printStackTrace();
+		}
+		return emails;
 	}
 
 	@Override
 	public void delete(Email email) throws RemoteException {
-		// TODO Auto-generated method stub
+		
 		
 	}
 	
