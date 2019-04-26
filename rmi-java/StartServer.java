df3e48b59fd66cca29220f60da1e80fc4d6fac70
Luis <luiscarlos.sf2017@gmail.com>
2019-04-25 21:50:56 -0300

    Add new message when server starts

--- rmi-java/StartServer.java
+++ rmi-java/StartServer.java
@@ -7,6 +7,7 @@ public class StartServer{
             System.out.println("Iniciando o servidor...");
             Registry registry = LocateRegistry.getRegistry();
             registry.rebind("ServerEmail", new ServerEmail());
+            System.out.println("Servidor inicializado com sucesso na porta " + registry.REGISTRY_PORT);
         }catch(Exception e){
             System.out.println("Ocorre um problema na inicialização do servidor.\n" + e.toString());
         }
